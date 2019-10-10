010f942f42e5885de3b090af4226e7a90baecb96
fernandocto.scz <fernandocto.scz@gmail.com>
2019-10-10 06:44:09 -0400

    Agregar rutas principales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: '/login', pathMatch: 'full'
+  },
+  {
+    path: 'dashboard', loadChildren: './components/dashboard/dashboard.module#DashboardModule'
+  },
+  {
+    path: 'login', component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
