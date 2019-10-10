970297130cb9d69d1208b402e76cf0080bbb6f46
fernandocto.scz <fernandocto.scz@gmail.com>
2019-10-10 06:47:03 -0400

    Agregar directiva de expand

---
+++ src/app/components/dashboard/directives/expand.directive.ts
@@ -0,0 +1,13 @@
+import { Directive, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appExpand]'
+})
+export class ExpandDirective {
+  @HostBinding('class.active') isOpen = false;
+  @HostListener('click', ['$event']) 
+  toggleOpen(e){
+    this.isOpen = !this.isOpen;
+    e.stopPropagation();
+  }
+}
