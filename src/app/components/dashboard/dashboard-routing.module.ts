ba493e6af058c3e2400ae9a40adc97e2869d5e80
fernandocto.scz <fernandocto.scz@gmail.com>
2019-10-10 06:48:35 -0400

    Agregar rutas y modulo principal del dashboard

---
+++ src/app/components/dashboard/dashboard-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SolicitudComponent } from './components/solicitud/solicitud.component';
+import { ReportesComponent } from './components/reportes/reportes.component';
+import { PersonalComponent } from './components/personal/personal.component';
+import { DashboardComponent } from './dashboard.component';
+
+const routes: Routes = [
+  {
+    path: '', component: DashboardComponent,
+    children: [
+      {
+        path: 'solicitud', component: SolicitudComponent
+      },
+      {
+        path: 'reportes', component: ReportesComponent
+      },
+      {
+        path: 'personal', component: PersonalComponent
+      },
+      { 
+        path: '', redirectTo: '/dashboard/solicitud', pathMatch: 'full'
+      },
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class DashboardRoutingModule { }
---
+++ src/app/components/dashboard/dashboard.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardRoutingModule } from './dashboard-routing.module';
+
+import { DashboardComponent } from './dashboard.component';
+
+import { SolicitudComponent } from './components/solicitud/solicitud.component';
+import { PersonalComponent } from './components/personal/personal.component';
+import { ReportesComponent } from './components/reportes/reportes.component';
+
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { ExpandDirective } from './directives/expand.directive';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    SolicitudComponent,
+    ReportesComponent,
+    PersonalComponent,
+    ExpandDirective
+  ],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    AngularFontAwesomeModule
+  ]
+})
+export class DashboardModule { }
