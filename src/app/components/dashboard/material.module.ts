a6f4760cc750e93ac9257a31c90b1ff42a1fc307
fernandocto.scz <fernandocto.scz@gmail.com>
2019-10-10 17:19:58 -0400

    Adicionar modulos de material

--- src/app/components/dashboard/material.module.ts
+++ src/app/components/dashboard/material.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 const MODULES = [
   MatToolbarModule,
-  MatButtonModule
+  MatButtonModule,
+  MatCardModule,
+  MatInputModule,
+  MatListModule,
+  MatIconModule,
+  MatMenuModule
 ]
 
 @NgModule({
