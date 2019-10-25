34e037669b9cbeab8cf238f1f87ead4dde289661
unknown <fernandocto.scz@gmail.com>
2019-10-25 03:53:32 -0400

    Arregar el componente solicitud

--- src/app/components/dashboard/components/solicitud/solicitud.component.ts
+++ src/app/components/dashboard/components/solicitud/solicitud.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./solicitud.component.scss']
 })
 export class SolicitudComponent implements OnInit {
-
+  public title = 'Solicitud'
+  public tabs = [
+    {
+      id: 1,
+      isActive: true,
+      name: 'Pendientes',
+      icon: 'tags'
+    },
+    {
+      id: 2,
+      isActive: false,
+      name: 'En progreso',
+      icon: 'cogs'
+    },
+    {
+      id: 3,
+      isActive: false,
+      name: 'Realizadas',
+      icon: 'check-circle'
+    }
+  ]
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
+  irTab(tab) {
+    this.tabs.map(item => item.isActive = item.id === tab.id ? true : false)
+  }
 }
