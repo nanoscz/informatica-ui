017f7d3bb363837bb99cb296a54510b7616e784f
unknown <fernandocto.scz@gmail.com>
2019-11-09 16:03:53 -0400

    Agregar servicio de storage

---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  constructor() { }
+
+  setData(key: string, data: string) {
+    localStorage.setItem(key, data);
+  }
+
+  getData(key: string) {
+    return JSON.parse(localStorage.getItem(key));
+  }
+
+  removeData(key: string) {
+    localStorage.removeItem(key);
+  }
+
+  clear() {
+    localStorage.clear();
+  }
+
+  /**  SESSION STORAGE */
+
+  setDataSession(key: string, data: string) {
+    sessionStorage.setItem(key, data);
+  }
+
+  getDataSession(key: string) {
+    return JSON.parse(sessionStorage.getItem(key));
+  }
+}
