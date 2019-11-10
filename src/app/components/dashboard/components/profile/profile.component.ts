5de178493178914ca267391c60f280f177750b93
unknown <fernandocto.scz@gmail.com>
2019-11-10 02:06:51 -0400

    Agregar componente de perfil

--- src/app/components/dashboard/components/profile/profile.component.ts
+++ src/app/components/dashboard/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  public data: any;
+  constructor(
+    private storageService: StorageService
+  ) { }
 
   ngOnInit() {
+    this.data = this.storageService.getData('token');
   }
 
 }
