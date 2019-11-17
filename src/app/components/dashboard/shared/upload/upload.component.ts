18519ffa4ad4836828335ceac29862af2a238df6
unknown <fernandocto.scz@gmail.com>
2019-11-17 15:15:50 -0400

    Agregar componente de upload

--- src/app/components/dashboard/shared/upload/upload.component.ts
+++ src/app/components/dashboard/shared/upload/upload.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadComponent implements OnInit {
 
+  public file: File
+  public imageTmp: any
+  public textButton = "Update Photo"
   constructor() { }
-
   ngOnInit() {
   }
 
+  selectImg(file: File) {
+    if(!file) {
+      this.file = null;
+      return
+    }
+    if(file.type.indexOf('image') < 0) {
+      alert("Error")
+      this.file = null
+      return
+    }
+    this.file = file
+    let reader = new FileReader()
+    let urlImageTemp =  reader.readAsDataURL(file)
+    reader.onloadend = () => {
+      this.imageTmp = reader.result
+      console.log(this.imageTmp)
+    }
+  }
 }
