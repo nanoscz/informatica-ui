9d137c80a3bd22e5caa23f5ccf332904ea252ce3
unknown <fernandocto.scz@gmail.com>
2019-11-17 16:45:46 -0400

    Agregar metodo getImage (uploadService)

--- src/app/services/upload.service.ts
+++ src/app/services/upload.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { handlerErrorPromise } from '../utils/handler-errors';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,15 @@ export class UploadService {
   constructor(private http: HttpClient) {
   }
 
+  getImage(image: string) {
+    return this.http.get(`${this.baseUrl}/${image}`)
+      .toPromise()
+      .catch(handlerErrorPromise);
+  }
+
   upload(file: File) {
     return this.http.post(`${this.baseUrl}/upload`, file)
       .toPromise()
-      .catch();
+      .catch(handlerErrorPromise);
   }
 }
